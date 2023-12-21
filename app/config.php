3bed89265b404c22f0e465c0da9af7a0763464a8
margauxargentin <margauxargentin@gmail.com>
2023-12-21 15:52:20 +0100

    feat: 🎸 Modify config to connect php to db

---
+++ app/config.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "db";
+$username = "iai3_05_user";
+$password = "P@ssw0rd";
+$dbname = "Nuage";
+
+try {
+  $mysqli = new mysqli($servername, $username, $password, $dbname);
+  $mysqli->set_charset("utf8mb4");
+} catch(Exception $e) {
+  error_log($e->getMessage());
+  exit('Error connecting to database'); //Should be a message a typical user could understand
+}
+?>

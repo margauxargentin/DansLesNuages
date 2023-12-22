890ad394498d5a67aa74924eee86bf0cf228013d
margauxargentin <margauxargentin@gmail.com>
2023-12-22 12:27:01 +0100

    docs: ✏️ Add comments to config.php

--- app/config.php
+++ app/config.php
@@ -1,9 +1,10 @@
 <?php
-$servername = "192.168.105.10";
+$servername = "192.168.105.10"; // Specify fixed IP of the database server
 $username = "iai3_05_user";
 $password = "P@ssw0rd";
 $dbname = "Nuage";
 
+// Create connection to database
 try {
   $mysqli = new mysqli($servername, $username, $password, $dbname);
   $mysqli->set_charset("utf8mb4");
